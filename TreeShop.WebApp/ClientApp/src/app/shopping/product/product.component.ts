98b455971e6b8ec1b37f57c5842db7d79aa1938d
TrungHieu <hieult25042000@gmail.com>
2023-05-17 17:12:11 +0700

    12313

--- TreeShop.WebApp/ClientApp/src/app/shopping/product/product.component.ts
+++ TreeShop.WebApp/ClientApp/src/app/shopping/product/product.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { SystemConstants } from 'src/app/core/common/system.constants';
+import { DataService } from 'src/app/core/services/data.service';
+import { NotificationService } from 'src/app/core/services/notification.service';
+import { PaginatorCustomService } from 'src/app/core/services/paginator-custom.service';
 
 @Component({
   selector: 'app-product',
@@ -6,11 +12,70 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
+  urlImage:any;
+  page = 0;
+  keyword: string = '';
+  totalRow: number = 0;
+  totalPage: number = 0;
+  pageSize = 6;
+  productBestSaler:any;
+  lstShopCart:any = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private spinner: NgxSpinnerService,
+    private pagin: PaginatorCustomService,
+    private formBuilder: FormBuilder,
+    private dataService: DataService,
+    private notificationService: NotificationService
+  ) {
+    this.urlImage = SystemConstants.URL_IMAGE;
   }
 
+  ngOnInit(): void {
+
+    this.loadProductBestSale();
+  }
+
+  loadProductBestSale(){
+      this.spinner.show();
+      this.dataService
+        .getShop(
+          'Product/getlistpaging_shop?page=' +
+          this.page +
+          '&pageSize=' +
+          this.pageSize +
+          '&keyword=' +
+          this.keyword +
+          '&bestSellers=true'
+        )
+        .subscribe(
+          (data: any) => {
+
+            this.productBestSaler = data.items;
+            this.totalRow = data.totalCount;
+            this.spinner.hide();
+          },
+          (err) => {
+            this.notificationService.printErrorMessage(
+              'Không tải được danh sách!'
+            );
+            this.spinner.hide();
+          }
+        );
+  }
+
+  addShopCart(productId:any){
+    let shopCart = localStorage.getItem(SystemConstants.SHOP_CART);
+    if(shopCart!=null){
+      let lst = JSON.parse(shopCart);
+      if(!lst.includes(productId)){
+        lst.push(productId);
+        localStorage.setItem(SystemConstants.SHOP_CART, JSON.stringify(lst));
+      }
+    }else{
+      this.lstShopCart.push(productId);
+      localStorage.setItem(SystemConstants.SHOP_CART, JSON.stringify(this.lstShopCart));
+    }
+  }
 
 }

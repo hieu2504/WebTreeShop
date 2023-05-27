85cf226c1ad987f6bebd3cdc698d34c5a4b2863a
TrungHieu <hieult25042000@gmail.com>
2023-05-27 11:28:59 +0700

    123123

--- TreeShop.WebApp/ClientApp/src/app/shopping/order-shop/order-shop.component.ts
+++ TreeShop.WebApp/ClientApp/src/app/shopping/order-shop/order-shop.component.ts
@@ -6,10 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-shop.component.css']
 })
 export class OrderShopComponent implements OnInit {
-
+  imgId = 1
   constructor() { }
 
   ngOnInit(): void {
+    const imgs = document.querySelectorAll('.img-select a');
+    const imgBtns = Array.from(imgs);
+     this.imgId = 1;
+
+imgBtns.forEach((imgItem:any) => {
+    imgItem.addEventListener('click', (event:any) => {
+        event.preventDefault();
+
+        this.imgId = imgItem.dataset.id;
+        this.slideImage();
+    });
+
+});
+
+
+
+
   }
+ slideImage(){
+
+    const displayWidth = document.querySelector('.img-showcase img:first-child')?.clientWidth;
+    const img = document.querySelector('.img-showcase') as HTMLImageElement;
+
+    img.style.transform = `translateX(${- (this.imgId - 1) * displayWidth!}px)`;
+}
 
 }
